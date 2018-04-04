7ad090a36dc39d58ff383480f58e4009d98f16b3
Moisés Aray <araymoises55@gmail.com>
2018-04-04 03:03:47 -0430

    avance

--- server.py
+++ server.py
@@ -26,19 +26,20 @@ y_brazo = 0
 class Mover(Resource):
     def get(self, x, y):
     	global x_brazo
+    	global y_brazo
         #x_brazo = x_brazo + int(x)
     	#GPIO.cleanup()
         if int(x)>x_brazo:
-            orientacion=1
-            rango_1 = 0
-            rango_2 = 4
+            x_orientacion=1
+            x_rango_1 = 0
+            x_rango_2 = 4
         else:
-            orientacion=-1
-            rango_1 = 4
-            rango_2 = -1
+            x_orientacion=-1
+            x_rango_1 = 4
+            x_rango_2 = -1
 
     	while x_brazo != int(x):
-    		for i in range(rango_1,rango_2,orientacion):
+    		for i in range(x_rango_1, x_rango_2, x_orientacion):
     			time.sleep(0.01)
     			if i==0:
     				GPIO.output(17, 1)
@@ -64,9 +65,48 @@ class Mover(Resource):
     				GPIO.output(27, 0)
     				GPIO.output(22, 1)
     				print "i es 3"
-    		x_brazo = x_brazo + orientacion
-    		print x_brazo
-	return x_brazo
+    		x_brazo = x_brazo + x_orientacion
+    		print "X: " + x_brazo
+
+        if int(x)>x_brazo:
+            x_orientacion=1
+            x_rango_1 = 0
+            x_rango_2 = 4
+        else:
+            x_orientacion=-1
+            x_rango_1 = 4
+            x_rango_2 = -1
+
+    	while y_brazo != int(y):
+    		for i in range(y_rango_1, y_rango_2, y_orientacion):
+    			time.sleep(0.01)
+    			if i==0:
+    				GPIO.output(17, 1)
+    				GPIO.output(18, 1)
+    				GPIO.output(27, 0)
+    				GPIO.output(22, 0)
+    				print "i es 0"
+    			if i==1:
+    				GPIO.output(17, 0)
+    				GPIO.output(18, 1)
+    				GPIO.output(27, 1)
+    				GPIO.output(22, 0)
+    				print "i es 1"
+    			if i==2:
+    				GPIO.output(17, 0)
+    				GPIO.output(18, 0)
+    				GPIO.output(27, 1)
+    				GPIO.output(22, 1)
+    				print "i es 2"
+    			if i==3:
+    				GPIO.output(17, 1)
+    				GPIO.output(18, 0)
+    				GPIO.output(27, 0)
+    				GPIO.output(22, 1)
+    				print "i es 3"
+    		y_brazo = y_brazo + y_orientacion
+    		print "Y: " + y_brazo
+	return x_brazo + "," + y_brazo
 
 class Tracks(Resource):
     def get(self):
